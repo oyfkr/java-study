f72474f8f1ab226b5491ac2d99f9140d43363180
DESKTOP-FF2H0VE\oghkr <oyfkr@naver.com>
2020-07-20 22:22:58 +0900

    if_first study

---
+++ src/if_fir.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class if_fir {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int a,b;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		
+		if(a == b) {
+			System.out.println("==");
+		}else if(a > b) {
+			System.out.println(" > ");
+		}else {
+			System.out.println(" < ");
+		}
+	}
+}
